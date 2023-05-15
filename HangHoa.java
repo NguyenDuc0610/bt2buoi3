7d876f02f5e0017bead356d36f56b9789a24fe4f
NguyenDuc0610 <119928685+NguyenDuc0610@users.noreply.github.com>
2023-05-15 17:17:51 +0700

    Create HangHoa.java

---
+++ HangHoa.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+
+abstract class HangHoa {
+    private String maHang;
+    private String tenHang;
+    private int soLuongTon;
+    private double donGia;
+
+    public HangHoa(String maHang, String tenHang, int soLuongTon, double donGia) {
+        this.maHang = maHang;
+        this.tenHang = tenHang;
+        this.soLuongTon = soLuongTon;
+        this.donGia = donGia;
+    }
+
+    public String getMaHang() {
+        return maHang;
+    }
+
+    public String getTenHang() {
+        return tenHang;
+    }
+
+    public int getSoLuongTon() {
+        return soLuongTon;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public abstract double tinhTienVAT();
+
+    public abstract String danhGiaBanRanh();
+}
